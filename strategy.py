21e485ea3a7f832ed0626b55af62c92021c5b29d
Ananya M <ananyagowda81@gmail.com>
2024-02-22 20:35:50 +0530

    Weighted function num_shares calculation

--- strategy.py
+++ strategy.py
@@ -60,22 +60,28 @@ def test_strategy(file, START_CASH=10000, AVG_DAYS=7, AVG_GAP=2, BUY_THRESHOLD=-
             sum += data[i+j+AVG_GAP+1][2]
         avg = sum / AVG_DAYS
         diff = price - avg
-        log(i, date, price, avg, diff, file)
-        if diff < BUY_THRESHOLD * avg:
-            log("buy")
-            if (cash > price):
-                trades += 1
-                cash -= 1*price
-                stock += 1
-                volume += price
+        # log(i, date, price, avg, diff, file)
 
-        if diff > SELL_THRESHOLD * avg:
-            log("sell")
-            if (stock > 0):
+        if (diff < 0):
+            amount = min(cash, BUY_THRESHOLD*diff)
+            num_shares = int(amount/price)
+            if (num_shares > 0):
                 trades += 1
-                cash += 1*price
-                stock -= 1
-                volume += price
+                cash -= num_shares * price
+                stock += num_shares
+                volume += num_shares*price
+                # log(f"[BUY] {num_shares}@{num_shares*price}")
+
+        if (diff > 0):
+            amount = SELL_THRESHOLD * diff
+            num_shares = min(int(amount/price), stock)
+            if (num_shares > 0):
+                trades += 1
+                cash += num_shares*price
+                stock -= num_shares
+                volume += num_shares*price
+                # log(f"[SELL] {num_shares}@{num_shares*price}")
+
     profit = int(cash + stock*data[0][2]) - START_CASH
     if profit > 0:
         log(
@@ -128,17 +134,18 @@ if __name__ == "__main__":
     Y2 = []
     MARKERS = ".,ov^<>12348spP*hH+xXDd|_"
 
-    for i in range(2, 9):
+    RANGE1 = range(2, 6)
+    for i in RANGE1:
         x = []
         y1 = []
         y2 = []
-        for j in range(1, 30):
-            var2 = j*0.1/100
-            var1 = i/2
+        for j in range(30, 60):
+            var1 = i
+            var2 = j*200
             print(f"Optimizing {var1}@{var2}")
-            c, p, init = run_all(start_cash=5000, avg_days=5,
-                                 avg_gap=0, buy_threshold=-1*var2, sell_threshold=var1*var2)
-            x.append(var2*100)
+            c, p, init = run_all(start_cash=var2, avg_days=var1,
+                                 avg_gap=0, buy_threshold=-2.5*1500, sell_threshold=1500)
+            x.append(var2)
             y1.append(100-100*c/init)
             y2.append(100*p/init)
         X.append(x)
@@ -147,8 +154,8 @@ if __name__ == "__main__":
 
     end_time = datetime.datetime.now()
     print(f"Time Taken: {end_time-start_time} s")
-    for i, x, y1, m in zip(range(2, 9), X, Y1, MARKERS):
-        plt.plot(x, y1, m+'b-', linewidth=i/3)
-    for i, x, y2, m in zip(range(2, 9), X, Y2, MARKERS):
-        plt.plot(x, y2, m+'r-', linewidth=i/3)
+    for i, x, y1, m in zip(RANGE1, X, Y1, MARKERS):
+        plt.plot(x, y1, m+'b-', linewidth=2*i/len(RANGE1))
+    for i, x, y2, m in zip(RANGE1, X, Y2, MARKERS):
+        plt.plot(x, y2, m+'r-', linewidth=2*i/len(RANGE1))
     plt.show()
